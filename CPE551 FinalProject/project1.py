342c3695e92747965d7ba46a603b7ae13339ccaa
JonasYen <89710851+JonasYen@users.noreply.github.com>
2021-12-16 10:56:17 -0800

    Add files via upload

--- CPE551 FinalProject/project1.py
+++ CPE551 FinalProject/project1.py
@@ -7,22 +7,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 
-# add your import statements
 executable_path = '/CPE551/FinalProject/driver/chromedriver'
-# address = 'your adress'
-# password = 'your password'
-# product_line = 'ThinkVision'
+
 page_url = 'http://www.pricegrabber.com/computer-monitors/browse/'
 results = []# variable to hold all reviews
 driver = webdriver.Chrome(executable_path=executable_path)
 limit = 3
 page = driver.get(page_url)
-# email = driver.find_element_by_css_selector('input.inputtext.login_form_input_box#email')
-# email.send_keys(address)
-# pw = driver.find_element_by_css_selector('input.inputtext.login_form_input_box#pass')
-# pw.send_keys(password)
-# searchB = driver.find_element_by_css_selector('label.login_form_login_button.uiButton.uiButtonConfirm#loginbutton')
-# searchB.click()
 
 for i in range(limit):
     temp = []
